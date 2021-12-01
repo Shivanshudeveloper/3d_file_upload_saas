aa0e210bf77182065703747397c56442db8cbb9a
Shivanshudeveloper <developer.shivanshu@gmail.com>
2021-12-01 09:15:45 +0530

    S

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MongoURI: 'mongodb+srv://adminuser:Testuser@1234@cluster0.iu6gm.mongodb.net/otherdatadb?retryWrites=true&w=majority'
+}
+
+// module.exports = {
+//     MongoURI: 'mongodb://localhost:27017/search'
+// }
+
